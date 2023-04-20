3c030ca51750f10792a469ed6e6f19fe085a0bf2
Sonia D Pagano Doval <113840141+SPaganoDoval@users.noreply.github.com>
2023-04-20 20:11:41 +0000

    Version modificada de Gitpod

--- src/models.py
+++ src/models.py
@@ -17,3 +17,58 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+     class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "hair_color": self.hair_color}
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "climate": self.climate}
+
+
+class UserFavoritePlanets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    favorite_planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'),  nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False )
+ 
+
+    def __repr__(self):
+        return '<UserFavoritePlanets %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "favorite_planet_id": self.id}
+    
+    
+
+class UserFavoritePeople(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    favorite_people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False )
+
+    def __repr__(self):
+        return '<UserFavoritePeople %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "favorite_people_id": self.id}   
